2bc5dbde6d6a30640ba215325f679f6a943b36ee
prashantbharati92@gmail.com <prashantbharati92@gmail.com>
2022-03-22 19:39:09 +0530

    cloned repo

--- GiveItem.js
+++ GiveItem.js
@@ -1,5 +1,5 @@
 const GiveItem = (props) => {
-  return <section>Give Item</section>;
+  return <section>it's working lol</section>;
 };
 
 const domContainer = document.querySelector("#injected-react-content");
